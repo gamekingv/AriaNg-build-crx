4f860d3e1a47b3b335340ada995b60471961833d
gamekingv <31853855+gamekingv@users.noreply.github.com>
2021-05-18 09:19:51 +0800

    Update parse.js

--- parse.js
+++ parse.js
@@ -5,7 +5,8 @@ const fs = require('fs').promises;
     const html = (await fs.readFile('source/index.html')).toString();
     const result = html
       .replace('</ul></li><li class="divider"></li><li><a class="toolbar"', '</ul></li><li class="divider"></li><li class="disabled" ng-class="{\'disabled\': !isTaskSelected()}" ng-controller="combineVideo"><a class="toolbar" title="合并视频" ng-click="combineSelectedVideo()"><i class="fa fa-compress"></i></a></li><li class="divider"></li><li><a class="toolbar"')
-      .replace('></body>', '><script src="js/combine-video.js"></script></body>');
+      .replace('></body>', '><script src="js/combine-video.js"></script></body>')
+      .replace('ng-href="#!/new"', 'href="#!/new"');
     await fs.writeFile('source/index.html', result);
   }
   catch (error) {
