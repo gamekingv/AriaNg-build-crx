a1c7d884126326d49ad763e0759245510251c8a4
gamekingv <793470218@qq.com>
2021-05-21 22:44:48 +0800

    Initialize

--- parse.js
+++ parse.js
@@ -5,9 +5,12 @@ const fs = require('fs').promises;
     const html = (await fs.readFile('source/index.html')).toString();
     const result = html
       .replace('</ul></li><li class="divider"></li><li><a class="toolbar"', '</ul></li><li class="divider"></li><li class="disabled" ng-class="{\'disabled\': !isTaskSelected()}" ng-controller="combineVideo"><a class="toolbar" title="合并视频" ng-click="combineSelectedVideo()"><i class="fa fa-compress"></i></a></li><li class="divider"></li><li><a class="toolbar"')
-      .replace('></body>', '><script src="js/combine-video.js"></script></body>')
-      .replace('ng-href="#!/new"', 'href="#!/new"');
+      .replace('></body>', '><script src="js/combine-video.js"></script></body>');
     await fs.writeFile('source/index.html', result);
+    const jsFiles = await fs.readdir('source/js');
+    const angularJS = jsFiles.find(file => file.includes('angular-packages'));
+    const js = (await fs.readFile(`source/js/${angularJS}`)).toString();
+    await fs.writeFile(`source/js/${angularJS}`, js.replace('"unsafe:"+', ''));
   }
   catch (error) {
     console.log(error);
